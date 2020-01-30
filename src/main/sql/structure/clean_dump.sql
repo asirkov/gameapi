a6497a9d7fb316bf3e78d493318ecb330a9a62f6
asirkov <51347504+asirkov@users.noreply.github.com>
2020-01-30 03:55:16 +0200

    add clean dump of db

---
+++ src/main/sql/structure/clean_dump.sql
@@ -0,0 +1,110 @@
+CREATE DATABASE  IF NOT EXISTS `gameapi` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `gameapi`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: gameapi
+-- ------------------------------------------------------
+-- Server version	5.7.28
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `game`
+--
+
+DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `participant_1_id` bigint(20) NOT NULL,
+  `participant_2_id` bigint(20) NOT NULL,
+  `result` enum('BLACK_WINS','WHITE_WINS','DRAW') DEFAULT NULL,
+  `start_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `participant_1_id_player_id_idx` (`participant_1_id`),
+  KEY `participant_2_id_player_id_idx` (`participant_2_id`),
+  CONSTRAINT `participant_1_id_player_id` FOREIGN KEY (`participant_1_id`) REFERENCES `player` (`id`) ON UPDATE CASCADE,
+  CONSTRAINT `participant_2_id_player_id` FOREIGN KEY (`participant_2_id`) REFERENCES `player` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `move`
+--
+
+DROP TABLE IF EXISTS `move`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `move` (
+  `id` bigint(20) NOT NULL,
+  `game_id` bigint(20) NOT NULL,
+  `move_type` enum('NONE','MOVE','KILL') NOT NULL,
+  `move_from` varchar(45) DEFAULT NULL,
+  `move_to` varchar(45) DEFAULT NULL,
+  `killed_checker` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `move_id_game_id_idx` (`game_id`),
+  CONSTRAINT `move_id_game_id` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `player`
+--
+
+DROP TABLE IF EXISTS `player`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `player` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `player_name` varchar(128) NOT NULL,
+  `games_count` bigint(20) DEFAULT NULL,
+  `wins_count` bigint(20) DEFAULT NULL,
+  `loses_count` bigint(20) DEFAULT NULL,
+  `avatar_data` blob NOT NULL,
+  `online` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `player_name_UNIQUE` (`player_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `login` varchar(128) NOT NULL,
+  `password_hash` varchar(128) NOT NULL,
+  `password_salt` varchar(128) NOT NULL,
+  `player_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login_UNIQUE` (`login`),
+  KEY `player_id_user_id_idx` (`player_id`),
+  CONSTRAINT `player_id_user_id` FOREIGN KEY (`player_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-01-30  3:52:33
